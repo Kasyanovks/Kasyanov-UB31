db9382c5b129edc7d78701e7cd164ce99972d8dc
Kasyanovks <konstantinkasyanovvrn@gmail.com>
2023-11-14 20:18:43 +0300

    Вариант 1 (Задание 1)

--- praktika.py
+++ praktika.py
@@ -1,7 +1,22 @@
-counter=0
-for a in range(1, 100):
-    for b in range(1, 100):
-        for c in range(1, 100):
-            for d in range(1, 100):
-                if a**3+b**3==c**3+d**3 and a != b and a != c and a != d and b!= c and b != d and c != d:
-                    print(a**3+b**3)
+# объявление функции
+def is_pangram(text):
+    s=text.lower()
+    for i in 'qwertyuiopasdfghjklzxcvbnm':
+        if i in s:
+            flag=True
+        else:
+            flag=False
+            break
+    if flag==True:
+        return True
+    else:
+        return False
+
+
+# считываем данные
+text = input()
+
+# вызываем функцию
+print(is_pangram(text))
+
+
