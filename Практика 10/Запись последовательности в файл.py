cb3202c96ee4571e3cd1e7cc9bdfe335f5d6b31e
Kasyanovks <145257248+Kasyanovks@users.noreply.github.com>
2023-11-27 18:26:10 +0300

    Update Запись последовательности в файл.py

--- Практика 10/Запись последовательности в файл.py
+++ Практика 10/Запись последовательности в файл.py
@@ -1,1 +1,16 @@
-
+from random import randint
+n = int(input())
+m = int(input())
+a = []
+for i in range(n):
+    b=[]
+    for j in range(m):
+        b.append(randint(1,100))
+    a.append(b)
+f = open(r'Програмирование.txt', 'w')
+for i in a:
+    for j in i:
+        f.write(str(j))
+        f.write(' ')
+    f.write("\n")
+f.close()
