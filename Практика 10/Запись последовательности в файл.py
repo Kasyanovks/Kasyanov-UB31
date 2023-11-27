a480f0b0a454f753efb8ec65ab3c1efc5dd2f008
Kasyanovks <145257248+Kasyanovks@users.noreply.github.com>
2023-11-27 18:28:45 +0300

    Update Запись последовательности в файл.py

--- Практика 10/Запись последовательности в файл.py
+++ Практика 10/Запись последовательности в файл.py
@@ -7,7 +7,7 @@ for i in range(n):
     for j in range(m):
         b.append(randint(1,100))
     a.append(b)
-f = open(r'Програмирование.txt', 'w')
+f = open(r'C:\Users\konst\OneDrive\Документы\Языки програмирования\Програмирование', 'w')
 for i in a:
     for j in i:
         f.write(str(j))
