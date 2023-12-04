277ffd56c3a5b4dfff5b402418a2886fcdca9285
Kasyanovks <145257248+Kasyanovks@users.noreply.github.com>
2023-12-04 19:25:20 +0300

    Update q.py

--- Практика 12/q.py
+++ Практика 12/q.py
@@ -1,1 +1,26 @@
+import requests
+import json
+from tkinter import *
+def information_button():
+    name_repository =text.get()
+    url = f"https://api.github.com/users/{name_repository}"
+    response = requests.get(url).json()
 
+    keys = ['company','created_at','email','id','name','url']
+    response_modified = {}
+    for element in keys:
+        response_modified[element] = response[element]
+
+    with open('information.txt','w') as file:
+        json.dump(response_modified,file,indent=3)
+window = Tk()
+window.title('Репозитории')
+window.configure(background='grey')
+window.geometry('300x300')
+lbl = Label(window, text='Введите название репозитория', width=30, height=1, bg='grey', font=45)
+lbl.grid(row=0, column=1, padx=10)
+text = Entry(window, width=20)
+text.grid(column=1, row=1, pady=20)
+button = Button(window ,text='Получить информацию',width=30,height=1, command=information_button)
+button.grid(row=3, column=1, pady=20)
+window.mainloop()
